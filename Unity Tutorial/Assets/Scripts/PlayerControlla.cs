c7a77d62dccc198e59275fc94d6e847327ca95d3
CriTicalSSX <sam.glendenning@btinternet.com>
2019-02-19 17:24:33 +0000

    Ball movement

---
+++ Unity Tutorial/Assets/Scripts/PlayerControlla.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlla : MonoBehaviour {
+
+    private float speed = 5;
+    private float movement = 0f;
+    private Rigidbody2D rigidbody;
+
+    // Use this for initialization
+    void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+
+        if (movement > 0f || movement < 0f)
+        {
+            rigidbody.velocity = new Vector2(movement * speed, rigidbody.velocity.y);
+        }
+    }
+}
