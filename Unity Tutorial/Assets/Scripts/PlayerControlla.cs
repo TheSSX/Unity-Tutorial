40eb2c874afcbf65e6c5e2cd3afa5be1e8d38e91
Sam Glendenning <sam.glendenning@btinternet.com>
2019-02-20 16:48:20 +0000

    Singular jumping

--- Unity Tutorial/Assets/Scripts/PlayerControlla.cs
+++ Unity Tutorial/Assets/Scripts/PlayerControlla.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class PlayerControlla : MonoBehaviour {
 
     private float speed = 5;
+    public float jumpspeed = 5f;
     private float movement = 0f;
     private Rigidbody2D rigidbody;
+    public Transform groundcheckpoint;
+    public float groundcheckradius;
+    public LayerMask groundlayer;
+    private bool isTouchingGround;
 
     // Use this for initialization
     void Start () {
@@ -16,11 +21,19 @@ public class PlayerControlla : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        //if the groundcheckpoint object's (child of ball) has an object in the Ground layer within its radius, then 
+        //ball is considered to have touched the ground
+        isTouchingGround = Physics2D.OverlapCircle(groundcheckpoint.position, groundcheckradius, groundlayer);
         movement = Input.GetAxis("Horizontal");
 
         if (movement > 0f || movement < 0f)
         {
             rigidbody.velocity = new Vector2(movement * speed, rigidbody.velocity.y);
+        }
+
+        if (Input.GetButtonDown("Jump") && isTouchingGround)
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpspeed);
         }
     }
 }
