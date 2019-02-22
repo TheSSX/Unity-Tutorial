c1f2a9048f496836bc2162c99559c726d1f0600d
CriTicalSSX <sam.glendenning@btinternet.com>
2019-02-22 12:36:49 +0000

    Camera follows Mario

---
+++ Unity Tutorial/Assets/Scripts/CameraControlla.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControlla : MonoBehaviour {
+
+    public GameObject player;
+    public float offset;
+    private Vector3 playerposition;
+    public float offsetsmoothing;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("mario");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+
+        playerposition = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+
+        if (player.transform.localScale.x > 0f)
+        {
+            playerposition = new Vector3(playerposition.x + offset, playerposition.y, playerposition.z);
+        }
+        else
+        {
+            playerposition = new Vector3(playerposition.x - offset, playerposition.y, playerposition.z);
+        }
+
+        transform.position = Vector3.Lerp(transform.position, playerposition, offsetsmoothing * Time.deltaTime);
+    }
+}
